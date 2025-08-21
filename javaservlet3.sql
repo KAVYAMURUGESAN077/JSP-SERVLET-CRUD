f7e580dbf3164ddd414da1bf6dfdaaf003e2dc9b
Kavya786 <kavyamurugesan7@gmail.com>
2025-08-21 10:07:34 +0530

    Add files via upload

---
+++ javaservlet3.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE servlet3;
+USE servlet3;
+
+CREATE TABLE foods (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    quantity Int  NOT NULL
+);
+select*from foods;
